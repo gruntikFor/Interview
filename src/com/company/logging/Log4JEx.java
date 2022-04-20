41fe90bd1a85fc23cb1219f6844e80d88dc8ec9a
gruntik <gruntov99@mail.ru>
2022-04-20 13:23:36 +0300

    feat: add log4j2

---
+++ src/com/company/logging/Log4JEx.java
@@ -0,0 +1,24 @@
+package com.company.logging;
+
+import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.io.File;
+
+public class Log4JEx {
+    public static void main(String[] args) {
+//        Configurator.setLevel(logger, Level.INFO);
+
+        LoggerContext context = (LoggerContext) LogManager.getContext();
+        context.setConfigLocation(new File("log4j2.xml").toURI());
+
+        Logger logger = context.getRootLogger();
+        Logger fileLogger = context.getLogger("files");
+
+        logger.info("hello console logger");
+        fileLogger.info("hello file logger");
+
+
+    }
+}
